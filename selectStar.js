2cbae9f63807fb4acc8d7aa78831455c45a160e3
Joey Zhu <joey.j.zhu@gmail.com>
2024-02-27 19:53:03 -0800

    Revert "fix"
    This reverts commit 03e92cd84018ba86c1fa15081e5c9467b27fa3a6.

--- selectStar.js
+++ selectStar.js
@@ -16,9 +16,10 @@ document.addEventListener('keydown', (event) => {
     document.body.style.cursor = ''; // Reset cursor
     document.removeEventListener('mouseover', highlightElement, false);
     document.removeEventListener('click', selectElement, false);
-    console.log("selectElement called", event);
+  
     // Now you have the selected element in event.target
     // You can send this information back to your extension or perform other actions
   }
   
   // Remember to add cleanup code to remove event listeners if needed
+  
